02eb5398d642399f84f021ed3c8258a12d59374a
apav-dev <apavlick@yext.com>
2024-07-30 08:41:39 -0400

    no accordion if no subpages

--- src/components/my-site/PageAccordionItem.tsx
+++ src/components/my-site/PageAccordionItem.tsx
@@ -23,6 +23,8 @@ const PageAccordionItem = ({
   const [isHovered, setIsHovered] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const hasSubPages = page.page && page.page.length > 0;
+
   useEffect(() => {
     if (isEditing && inputRef.current) {
       inputRef.current.focus();
@@ -52,22 +54,36 @@ const PageAccordionItem = ({
     }
   };
 
+  const renderTriggerContent = () => (
+    <div className="px-4 flex items-center w-full">
+      <span>{page.title}</span>
+      {isHovered && (
+        <button onClick={handleEditClick} className="ml-2">
+          <Pencil size={16} />
+        </button>
+      )}
+    </div>
+  );
+
   return (
-    <AccordionItem className="" value={page.title}>
+    <AccordionItem className="" value={page.title} disabled={!hasSubPages}>
       {!isEditing ? (
-        <AccordionTrigger
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <div className="px-4 flex items-center w-full">
-            <span>{page.title}</span>
-            {isHovered && (
-              <button onClick={handleEditClick} className="ml-2">
-                <Pencil size={16} />
-              </button>
-            )}
+        hasSubPages ? (
+          <AccordionTrigger
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            {renderTriggerContent()}
+          </AccordionTrigger>
+        ) : (
+          <div
+            className="flex py-4"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            {renderTriggerContent()}
           </div>
-        </AccordionTrigger>
+        )
       ) : (
         <div className="flex items-center p-4">
           <input
@@ -86,33 +102,35 @@ const PageAccordionItem = ({
           </button>
         </div>
       )}
-      <AccordionContent>
-        <ul role="list" className="divide-y divide-gray-100">
-          {page.page?.map((page) => (
-            <li
-              key={page.meta.id}
-              className="flex items-center justify-between gap-x-6 py-2 px-2"
-            >
-              <div className="min-w-0 pl-8">
-                <div className="flex items-start gap-x-3">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    {page.c_pageTitle}
-                  </p>
+      {hasSubPages && (
+        <AccordionContent>
+          <ul role="list" className="divide-y divide-gray-100">
+            {page.page?.map((page) => (
+              <li
+                key={page.meta.id}
+                className="flex items-center justify-between gap-x-6 py-2 px-2"
+              >
+                <div className="min-w-0 pl-8">
+                  <div className="flex items-start gap-x-3">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                      {page.c_pageTitle}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-none items-center gap-x-4">
-                <a
-                  href={page.slug}
-                  className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
-                >
-                  Go To Page
-                  <span className="sr-only">, {page.c_pageTitle}</span>
-                </a>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </AccordionContent>
+                <div className="flex flex-none items-center gap-x-4">
+                  <a
+                    href={page.slug}
+                    className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+                  >
+                    Go To Page
+                    <span className="sr-only">, {page.c_pageTitle}</span>
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </AccordionContent>
+      )}
     </AccordionItem>
   );
 };
